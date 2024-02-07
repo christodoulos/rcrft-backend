d89c265185ff52cccd2227905517689566fc4655
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2024-02-07 16:32:56 +0200

    requirements

--- src/config.py
+++ src/config.py
@@ -1,5 +1,7 @@
-MONGO_HOST = 'localhost'
-MONGO_PORT = 27097
-MONGO_DBNAME = 'rcrft'
-GOOGLE_AUDIENCE = "614939923930-8embf5o9vgr3oiq66ma72rcm36ibsuqq.apps.googleusercontent.com"
+MONGO_HOST = "localhost"
+MONGO_PORT = 27017
+MONGO_DBNAME = "rcrft"
+GOOGLE_AUDIENCE = (
+    "614939923930-8embf5o9vgr3oiq66ma72rcm36ibsuqq.apps.googleusercontent.com"
+)
 JWT_SECRET_KEY = "rQY3B8WQvrqMFVhqXKq9VhGUrtreYCdMhQ"
