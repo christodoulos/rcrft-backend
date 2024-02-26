5832afa2c268ac7d1cfa7d2b70316c3f5890f78a
Spyros Tsattalios <spyros.tsattalios@gmail.com>
2024-02-26 17:36:14 +0200

    enums new commit

--- src/enums.py
+++ src/enums.py
@@ -2,7 +2,11 @@ from enum import Enum
 
 
 class DemoSite(Enum):
-    ATHENS = 'Athens, Greece'
-    BERLIN = 'Berlin, Germany'
-    BARCELONA = 'Barcelona, Spain'
-    NONE = 'Not specified'
+    BERLIN = "Berlin-Brandenburg, DE"
+    CATALONIA = "Coast of Catalonia, ES"
+    ATTICA = "Region of Attica, GR"
+    ZEELAND = "Province of Zeeland, NL"
+    TROMS = "Troms & Finnmark, NO"
+    ZEMGALE = "Zemgale region, LV"
+    LAGHI = "Valle dei Laghi area, IT"
+    NONE = "Not specified"
